e9773f9771c8266dea11ed0611c1d8499bb17437
Changgg <tanaikchang@gmail.com>
2022-09-23 23:59:37 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="flex bg-gradient-to-r from-[#f0c1fd] to-[#aaffff] w-full h-screen text-3xl">
+      <div className='flex flex-1 items-center justify-center'>Hello World</div>
+    </div>
+  )
+}
+
+export default App
