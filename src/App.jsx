c416f3aceb8778f04e909c33b9428f0415081a84
Changgg <tanaikchang@gmail.com>
2022-09-24 00:05:16 +0800

    added slect none

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="flex bg-gradient-to-r from-[#f0c1fd] to-[#aaffff] w-full h-screen text-3xl">
-      <div className='flex flex-1 items-center justify-center'>Hello World</div>
+      <div className='flex flex-1 items-center justify-center select-none'>Hello World</div>
     </div>
   )
 }
